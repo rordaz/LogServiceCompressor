77d776f2e7650eeb5066000bc33512fa29cda05f
rordaz <rafael.ordaz@gmail.com>
2022-12-08 08:29:24 -0600

    12082022 0829

---
+++ LogFilesServiceCompressor.Test/TestUtilities.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace LogFilesServiceCompressor
+{
+    internal class TestUtilities
+    {
+        public static void UnZipFiles(string zipPathAndFile, string outputFolder, string password = null, bool deleteZipFile = false)
+        {
+            
+            string testZipPath = Path.Combine(Directory.GetCurrentDirectory(), zipPathAndFile);
+            ZipInputStream s = new ZipInputStream(File.OpenRead(testZipPath));
+            if (password != null && password != String.Empty)
+                s.Password = password;
+            ZipEntry theEntry;
+            string tmpEntry = String.Empty;
+            while ((theEntry = s.GetNextEntry()) != null)
+            {
+                string directoryName = outputFolder;
+                string fileName = Path.GetFileName(theEntry.Name);
+                // create directory 
+                if (directoryName != "")
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+                if (fileName != String.Empty)
+                {
+                    if (theEntry.Name.IndexOf(".ini") < 0)
+                    {
+                        string fullPath = directoryName + "\\" + theEntry.Name;
+                        fullPath = fullPath.Replace("\\ ", "\\");
+                        string fullDirPath = Path.GetDirectoryName(fullPath);
+                        if (!Directory.Exists(fullDirPath)) Directory.CreateDirectory(fullDirPath);
+                        FileStream streamWriter = File.Create(fullPath);
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        streamWriter.Close();
+                    }
+                }
+            }
+            s.Close();
+            if (deleteZipFile)
+                File.Delete(zipPathAndFile);
+        }
+    }
+}
